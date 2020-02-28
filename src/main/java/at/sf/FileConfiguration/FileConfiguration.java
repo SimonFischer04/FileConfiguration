6efec7316df7bc39429ec895dee502a0c0cb32b3
SimonFischer04 <simi.fischa@gmail.comw>
2020-02-28 17:59:57 +0100

    fixed storing Objects, Number, Character, Boolean. refactoring

--- src/main/java/at/sf/FileConfiguration/FileConfiguration.java
+++ src/main/java/at/sf/FileConfiguration/FileConfiguration.java
@@ -42,35 +42,48 @@ public class FileConfiguration {
     }
 
     public boolean contains(String key) {
-        return get(key, String.class) != null;
+        return getJsonPrimitive(key) != null;
     }
 
     public <T> T get(String key, Class<T> classOfT) {
+        return gson.fromJson(getString(key), classOfT);
+    }
+
+    public String getString(String key) {
+        JsonPrimitive p = getJsonPrimitive(key);
+        if (p == null)
+            return null;
+        return p.getAsString();
+    }
+
+    public Integer getInt(String key) {
+        return getJsonPrimitive(key).getAsInt();
+    }
+
+    public Double getDouble(String key) {
+        return getJsonPrimitive(key).getAsDouble();
+    }
+
+    public Boolean getBoolean(String key){
+        return getJsonPrimitive(key).getAsBoolean();
+    }
+
+    public JsonObject getJsonObject(String key) {
         String[] subTypes = key.split("\\.");
         JsonObject object = jsonObject;
-        for (int i = 0; i < subTypes.length - 1; i++) {
+        for (int i = 0; i < subTypes.length; i++) {
             if (!object.has(subTypes[i]) && !(object.get(subTypes[i]) instanceof JsonObject)) {
                 return null;
             }
             object = object.get(subTypes[i]).getAsJsonObject();
         }
-        return gson.fromJson(object.get(subTypes[subTypes.length - 1]), classOfT);
+        return object;
     }
 
-    public String getString(String key) {
-        return get(key, String.class);
-    }
-
-    public Integer getInt(String key) {
-        return get(key, int.class);
-    }
-
-    public Double getDouble(String key) {
-        return get(key, double.class);
-    }
-
-    public JsonPrimitive getAsJsonPrimitive(String key) {
-        return get(key, JsonPrimitive.class);
+    public JsonPrimitive getJsonPrimitive(String key) {
+        String[] subTypes = key.split("\\.");
+        JsonObject object = getJsonObject(key.substring(0, key.lastIndexOf(".")));
+        return gson.fromJson(object.get(subTypes[subTypes.length - 1]), JsonPrimitive.class);
     }
 
     public void set(String key, Object value) {
@@ -92,9 +105,23 @@ public class FileConfiguration {
             }
             currObject = currObject.get(subtypes[i]).getAsJsonObject();
         }
-        currObject.addProperty(subtypes[subtypes.length - 1], gson.toJson(value));
 
-        if(autoSave){
+        /*
+            Store as right type
+         */
+        if (value instanceof Number) {
+            currObject.addProperty(subtypes[subtypes.length - 1], (Number) value);
+        } else if(value instanceof String) {
+            currObject.addProperty(subtypes[subtypes.length - 1], (String) value);
+        } else if(value instanceof Boolean) {
+            currObject.addProperty(subtypes[subtypes.length - 1], (Boolean) value);
+        } else if(value instanceof Character) {
+            currObject.addProperty(subtypes[subtypes.length - 1], (Character) value);
+        }else{
+            currObject.addProperty(subtypes[subtypes.length - 1], gson.toJson(value));
+        }
+
+        if (autoSave) {
             save();
         }
     }
@@ -106,6 +133,13 @@ public class FileConfiguration {
             bw.flush();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void clear() {
+        jsonObject = new JsonObject();
+        if (autoSave) {
+            save();
         }
     }
 }
--- src/main/java/at/sf/FileConfiguration/Main.java
+++ src/main/java/at/sf/FileConfiguration/Main.java
@@ -1,26 +1,49 @@
 package at.sf.FileConfiguration;
 
-import com.google.gson.JsonPrimitive;
+import com.google.gson.*;
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         //new JsonObject()
         System.out.println(System.getProperty("user.dir"));
-        FileConfiguration c = new FileConfiguration("test.json", true, true);
-        //c.set("test", "lol");
+        FileConfiguration conf = new FileConfiguration("test.json", true, true);
+        conf.clear();
 
-        //int i = c.get("a.c.d", int.class);
-        JsonPrimitive p = c.getAsJsonPrimitive("a.b.c.d");
-        System.out.println();
+        conf.set("a.b.c.testPoint", new Point(1, 2));
+        Point p = conf.get("a.b.c.testPoint", Point.class);
+        System.out.println("Point p = " + p);
 
-        c.set("a.b.c.ee.f", 1);
-        String s = c.get("a.b.c.d", String.class);
+        conf.set("a.b.testString", "teeesssttt");
+        System.out.println("conf.getString(\"a.b.testString\") = " + conf.getString("a.b.testString"));
+        System.out.println("conf.get(\"a.b.testString\", String.class) = " + conf.get("a.b.testString", String.class));
 
-        System.out.println();
-        //c.set("a.b.c", "hallo");
+        JsonObject o = conf.getJsonObject("a.b");
+        System.out.println(String.format("---\nJsonObject o = \n%s\n---", gson.toJson(o)));
+        System.out.println("o.get(\"testString\").getAsString() = " + o.get("testString").getAsString());
 
-        //c.save();
+        JsonPrimitive primitive = conf.getJsonPrimitive("a.b.testString");
+        System.out.println("primitive.getAsString() = " + primitive.getAsString());
+
+        conf.set("a.b.testInt", 42);
+        int i = conf.getInt("a.b.testInt");
+        System.out.println("int i = " + i);
+
+        conf.set("a.b.testDouble", 10 / 3.0);
+        double d = conf.getDouble("a.b.testDouble");
+        System.out.println("double d = " + d);
+
+        conf.set("a.b.testBoolean", true);
+        boolean b = conf.getBoolean("a.b.testBoolean");
+        System.out.println("boolean b = " + b);
+
+        conf.set("a.b.testChar", 'A');
+        char c = conf.getJsonPrimitive("a.b.testChar").getAsCharacter();
+        System.out.println("char c = " + c);
+        String cs = conf.getString("a.b.testChar");
+        System.out.println("String cs = " + cs);
 
     }
 }
